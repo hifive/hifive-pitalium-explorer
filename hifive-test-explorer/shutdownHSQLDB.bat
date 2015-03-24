34da370e5c43591be8a1b044b04e86953677abe0
sasaki <sasaki.tatsuya.8hr@jp.nssol.nssmc.com>
2015-03-24 11:30:32 +0900

    move the batch file and the settings of hsqldb.

--- hifive-test-explorer/shutdownHSQLDB.bat
+++
@@ -1,2 +0,0 @@
-@echo Must set the JAVA_HOME environment variable to point to the JRE or JDK.
-java -jar lib/sqltool-2.3.2.jar --rcfile src/main/resources/appConf/sqltool.rc --sql SHUTDOWN; testexplorer
--- hifive-test-explorer/startHSQLDB.bat
+++
@@ -1,2 +0,0 @@
-@echo Must set the JAVA_HOME environment variable to point to the JRE or JDK.
-java -cp lib/hsqldb-2.3.2.jar org.hsqldb.Server --props src/main/resources/appConf/hsqldb.properties
