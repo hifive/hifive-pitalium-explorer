b93451e21ad99b3af7b989abc8c474dcc76b15cd
ChanMin Kim <kcm1700@gmail.com>
2015-03-29 05:47:39 +0900

    Implement EdgeDetector class.
    - used Canny Edge Detection Algorithm.

---
+++ hifive-test-explorer/src/main/java/com/htmlhifive/testexplorer/image/Coordinate2D.java
@@ -0,0 +1,10 @@
+package com.htmlhifive.testexplorer.image;
+
+public class Coordinate2D<T> {
+    public T x, y;
+    public Coordinate2D(T x, T y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
