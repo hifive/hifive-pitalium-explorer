a68929e0cd168dbb26569b184c60b9929d053db8
Gyumin Sim <sim0629@gmail.com>
2015-06-08 13:11:00 +0900

    Remove unused import in TestExecution.java

--- hifive-test-explorer/src/main/java/com/htmlhifive/testexplorer/entity/TestExecution.java
+++ hifive-test-explorer/src/main/java/com/htmlhifive/testexplorer/entity/TestExecution.java
@@ -3,7 +3,6 @@ package com.htmlhifive.testexplorer.entity;
 import java.io.Serializable;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.util.TimeZone;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
