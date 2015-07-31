79e5e04282eac2095ef721c604d3aea52a2c580d
sasaki <sasaki.tatsuya.8hr@jp.nssol.nssmc.com>
2015-07-31 14:10:51 +0900

    #8 Areaテーブルへの検索メソッドの追加
    検索条件 TargetId で Area テーブルから検索を行うメソッドを追加しました。
    検索条件 TargetId, Excluded で Area テーブルから検索を行うメソッドを追加しました。

---
+++ hifive-test-explorer/src/main/java/com/htmlhifive/testexplorer/entity/AreaRepository.java
@@ -0,0 +1,13 @@
+package com.htmlhifive.testexplorer.entity;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface AreaRepository extends JpaRepository<Area, Integer> {
+
+	public List<Area> findByTargetId(@Param("targetId") Integer targetId);
+	
+	public Area getByTargetIdAndExcluded(@Param("targetId") Integer targetId, @Param("excluded") Boolean excluded);
+}
