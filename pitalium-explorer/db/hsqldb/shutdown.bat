725e24385ff087ecab5cc8eeb83578848bb1a896
sasaki <sasaki.tatsuya.8hr@jp.nssol.nssmc.com>
2015-08-05 20:37:13 +0900

    #14 DBの設定内にあるtestexplorerをpitalium-explorerに変更

--- pitalium-explorer/db/hsqldb/shutdown.bat
+++ pitalium-explorer/db/hsqldb/shutdown.bat
@@ -1,2 +1,2 @@
 @echo Must set the JAVA_HOME environment variable to point to the JRE or JDK.
-java -jar ../../lib/sqltool-2.3.2.jar --rcfile sqltool.rc --sql SHUTDOWN; testexplorer
+java -jar ../../lib/sqltool-2.3.2.jar --rcfile sqltool.rc --sql SHUTDOWN; pitalium-explorer
