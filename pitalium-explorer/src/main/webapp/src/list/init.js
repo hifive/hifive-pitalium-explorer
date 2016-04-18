ab925ce5fb8f404dfcb0a42c5d03efcaeee7394e
Minyoung Jun <minyoung9353@gmail.com>
2016-04-18 22:31:15 +0900

    body, navbar css file modified, color, padding, font design

--- pitalium-explorer/src/main/webapp/src/list/init.js
+++ pitalium-explorer/src/main/webapp/src/list/init.js
@@ -6,3 +6,30 @@ $(function() {
 	h5.core.controller('body>div.container',
 			hifive.pitalium.explorer.controller.ResultListPageController);
 });
+
+
+$(window).scroll(function () {
+	var height = $(document).scrollTop();
+	if (height > 80) {
+		
+		$(".navbar").fadeIn();
+		
+		
+	} else{
+		
+		$(".navbar").fadeOut();
+		
+	}
+	
+	if(height < 86){
+		
+		$(".background-pattern").fadeIn();
+		
+	}else{
+		$(".background-pattern").fadeOut();
+	}
+	
+	
+	
+});
+
