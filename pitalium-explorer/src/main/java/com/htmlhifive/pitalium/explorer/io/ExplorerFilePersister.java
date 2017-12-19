927988ce588def1e7329ccb22bd0526594b9ba2c
Huy <huy911990@gmail.com>
2017-12-19 15:58:36 +0700

    FileWriterをOutputStreamWriterに置き換

--- pitalium-explorer/src/main/java/com/htmlhifive/pitalium/explorer/io/ExplorerFilePersister.java
+++ pitalium-explorer/src/main/java/com/htmlhifive/pitalium/explorer/io/ExplorerFilePersister.java
@@ -238,9 +238,9 @@ public class ExplorerFilePersister extends FilePersister implements ExplorerPers
 			pairResultList.add(result);
 
 			try {
-				FileWriter fw = new FileWriter(filenamePairJson.getPath());
-				fw.write(JSONUtils.toString(comparedRectangles));
-				fw.close();
+				OutputStreamWriter sw = new OutputStreamWriter(new FileOutputStream(filenamePairJson.getPath()), StandardCharsets.UTF_8);
+				sw.write(JSONUtils.toString(comparedRectangles));
+				sw.close();
 			} catch (Exception e) {
 				log.error("file write error: can not write " + filenamePairJson.getPath());
 			}
@@ -250,9 +250,9 @@ public class ExplorerFilePersister extends FilePersister implements ExplorerPers
 		resultList.add(resultListOfExpected);
 
 		try {
-			FileWriter fw = new FileWriter(resultListJson.getPath());
-			fw.write(JSONUtils.toString(resultList));
-			fw.close();
+			OutputStreamWriter sw = new OutputStreamWriter(new FileOutputStream(resultListJson.getPath()), StandardCharsets.UTF_8);
+			sw.write(JSONUtils.toString(resultList));
+			sw.close();
 		} catch (Exception e){
 			log.error("file write error: can not write " + resultListJson.getPath());
 		}
@@ -434,9 +434,9 @@ public class ExplorerFilePersister extends FilePersister implements ExplorerPers
 		}
 
 		try {
-			FileWriter fw = new FileWriter(resultListJson.getPath());
-			fw.write(JSONUtils.toString(resultList));
-			fw.close();
+			OutputStreamWriter sw = new OutputStreamWriter(new FileOutputStream(resultListJson.getPath()), StandardCharsets.UTF_8);
+			sw.write(JSONUtils.toString(resultList));
+			sw.close();
 		} catch (Exception e) {
 			log.error("file write error: can not write " + resultListJson.getPath());
 		}
